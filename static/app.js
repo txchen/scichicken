b91e22538854fc923f7a37773d06324ce547571d
abe186186 <34462892+abe186186@users.noreply.github.com>
2018-04-21 13:17:21 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -114,7 +114,7 @@ function getMapSource (mapType) {
     ? 'erangel/v11'
     : 'miramar/v5'
   // if false, will use https://tiles2-v2.pubgmap.net/tiles/erangel/v11/{z}/{x}/{y}.png not sure if it is stable or not. But it will have more zoom, up to 5. Local only has up to 4
-  let useLocalResource = true
+  let useLocalResource = false
   const mapBase = useLocalResource
     ? '../maptiles'
     : 'https://tiles2-v2.pubgmap.net/tiles'
